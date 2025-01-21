27be286c5d5b5d57505421e04bbe3f9e282d235f
SydNichols <sydneynichols.m@gmail.com>
2025-01-21 17:43:23 -0500

    Added extra credit functionalities
    Added keyboard functionality and style changes, documenting them in the readme file
    
    Created the README.md file

---
+++ README.md
@@ -0,0 +1,11 @@
+#Style Changes
+
+Welcome to may gameshow app! Project 4 for my Treehouse techdegree. To make the project more interesting, I made some tyle changes to the game. 
+
+Hope you enjoy!
+
+- Changed the background to a pretty gradient using these colors: 
+    - Top color: #f4fafc (light blue/white)
+    - Middle color: #f3b7ad (soft pink)
+    - Bottom color: #93aec1 (grayish blue)
+- Changed the heart color using CSS to make them a reddish-orange color
--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -34,3 +34,20 @@ document.getElementById('btn__reset').addEventListener('click', () => {
     game = new Game();
     game.startGame();
 });
+
+//adds functionality for extra cred point - having the keyboard interact with the game
+document.addEventListener('keyup', (e) => {
+    //will only run if the game is in progress (overlay is gone)
+    if(game && document.getElementById('overlay').style.display === 'none') {
+        const keyboardButtons = document.querySelectorAll('.key');
+
+        const buttonPressed = Array.from(keyboardButtons).find(button => 
+            button.textContent === e.key.toLowerCase() &&
+            !button.disabled
+        );
+
+        if (buttonPressed) {
+            game.handleInteraction(buttonPressed)
+        }
+    }
+})
